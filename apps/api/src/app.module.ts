ccff61d68e30aaf1f9ce7410e821ebdb3d40a6cf
mahdi-i <mahdi1750idham@gmail.com>
2026-06-21 02:02:35 +0330

    refactor auth

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,35 +1,8 @@
-import { AuthModule } from '@mguay/nestjs-better-auth';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
-import { auth } from '@core/auth/auth';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { initializeDataSource } from '@shared/validation/initializeDataSource';
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres' as const,
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        synchronize: true,
-        entities: ['dist/typeorm/entities/**/*.js', 'dist/**/*.entity.js'],
-        dataSourceFactory: initializeDataSource,
-      }),
-    }),
-    AuthModule.forRootAsync({
-      useFactory: () => ({
-        auth,
-      }),
-    }),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' })],
   controllers: [],
   providers: [],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { initSwagger } from './common/config/swagger.config';
-import { AllExceptionsFilter } from './common/filter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalFilters(new AllExceptionsFilter());
-  initSwagger(app);
+
   await app.listen(3001);
 }
 bootstrap();
